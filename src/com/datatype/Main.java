2eda9ff8a12d4e3c67f1a93d52e7d195bd96611d
2oublePeace <vova.emiryan@yandex.ru>
2021-07-15 13:01:33 +0300

    inital commit

---
+++ src/com/datatype/Main.java
@@ -0,0 +1,8 @@
+package com.datatype;
+import static com.datatype.Task1.checkStatement;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(checkStatement(true, false, true, true));
+    }
+}
