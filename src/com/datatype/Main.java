be0f073e7dbfedecfbc991253d068377268e2485
2oublePeace <vova.emiryan@yandex.ru>
2021-07-19 15:55:43 +0400

    fix static class

--- src/com/datatype/Main.java
+++ src/com/datatype/Main.java
@@ -1,6 +1,6 @@
 package com.datatype;
 
-import com.datatype.TaskSolving.Methods;
+import com.datatype.TaskSolving;
 import java.util.Scanner;
 
 public class Main {
@@ -13,22 +13,22 @@ public class Main {
                 case 0:
                     return;
                 case 1:
-                    System.out.println("Two arguments is true: " + Methods.checkStatement(true, false, true, false));
+                    System.out.println("Two arguments is true: " + TaskSolving.checkStatement(true, false, true, false));
                     break;
                 case 2:
-                    System.out.println("Result of flipBit method: " + Methods.flipBit(34, 5));
+                    System.out.println("Result of flipBit method: " + TaskSolving.flipBit(34, 5));
                     break;
                 case 3:
-                    System.out.println("Count of leap years: " + Methods.countLeap(100));
+                    System.out.println("Count of leap years: " + TaskSolving.countLeap(100));
                     break;
                 case 4:
-                    System.out.println("Result symbol from slash: " + Methods.distanceToSlash(32));
+                    System.out.println("Result symbol from slash: " + TaskSolving.distanceToSlash(32));
                     break;
                 case 5:
-                    System.out.println("Is two grade: " + Methods.isTwoGrade(-8));
+                    System.out.println("Is two grade: " + TaskSolving.isTwoGrade(-8));
                     break;
                 case 6:
-                    System.out.println("Is palindrome" + Methods.isPalindrome("Madam, I'm Adam!"));
+                    System.out.println("Is palindrome: " + TaskSolving.isPalindrome("Madam, I'm Adam!"));
                     break;
             }
         }
--- src/com/datatype/TaskSolving.java
+++ src/com/datatype/TaskSolving.java
@@ -6,68 +6,66 @@ package com.datatype;
  * @author    Эмирян Владимир
  */
 public class TaskSolving {
-    static class Methods {
 
-        /**
-         * Метод реализующий решение задачи 1
-         * @return являются ли только два аругумента со значением true
-         */
-        static boolean checkStatement(boolean term1, boolean term2, boolean term3, boolean term4) {
-            //При конвертации в числа, сумма термов должна быть равна 2
-            return booleanToInt(term1) + booleanToInt(term2) + booleanToInt(term3) + booleanToInt(term4) == 2;
-        }
+    /**
+     * Метод реализующий решение задачи 1
+     * @return являются ли только два аругумента со значением true
+     */
+    static boolean checkStatement(boolean term1, boolean term2, boolean term3, boolean term4) {
+        //При конвертации в числа, сумма термов должна быть равна 2
+        return booleanToInt(term1) + booleanToInt(term2) + booleanToInt(term3) + booleanToInt(term4) == 2;
+    }
 
-        /**
-         * Вспомогательный метод, конвертирующий true и false в 1 и 0 соответственно
-         * @return 1 или 0 в соответствии с true/false
-         */
-        static int booleanToInt(boolean term) {
-            return (Boolean.toString(term).equals("true")) ? 1 : 0;
-        }
+    /**
+     * Вспомогательный метод, конвертирующий true и false в 1 и 0 соответственно
+     * @return 1 или 0 в соответствии с true/false
+     */
+    static int booleanToInt(boolean term) {
+        return (Boolean.toString(term).equals("true")) ? 1 : 0;
+    }
 
-        /**
-         * Метод реализующий решение задачи 2
-         * @return количество високосных годов
-         */
-        static int countLeap(int year) {
-            return year / 4 - year / 100;
-        }
+    /**
+     * Метод реализующий решение задачи 2
+     * @return количество високосных годов
+     */
+    static int countLeap(int year) {
+        return year / 4 - year / 100;
+    }
 
-        /**
-         * Метод реализующий решение задачи 3
-         * @return число с иныертированным битом
-         */
-        static int flipBit(int number, int position) {
-            char[] bitsOfNumber = Integer.toBinaryString(number).toCharArray();
-            bitsOfNumber[bitsOfNumber.length - position] = bitsOfNumber[position] == '0' ? '1' : '0';
-            int result = Integer.parseInt(String.valueOf(bitsOfNumber), 2);
-            return result;
-        }
+    /**
+     * Метод реализующий решение задачи 3
+     * @return число с иныертированным битом
+     */
+    static int flipBit(int number, int position) {
+        char[] bitsOfNumber = Integer.toBinaryString(number).toCharArray();
+        bitsOfNumber[bitsOfNumber.length - position] = bitsOfNumber[position] == '0' ? '1' : '0';
+        int result = Integer.parseInt(String.valueOf(bitsOfNumber), 2);
+        return result;
+    }
 
-        /**
-         * Метод реализующий решение задачи 4
-         * @return символ на определенном расстоянии от символа"\"
-         */
-        static char distanceToSlash(int a) {
-            return (char) ('\\' + a);
-        }
+    /**
+     * Метод реализующий решение задачи 4
+     * @return символ на определенном расстоянии от символа"\"
+     */
+    static char distanceToSlash(int a) {
+        return (char) ('\\' + a);
+    }
 
-        /**
-         * Метод реализующий решение задачи 5
-         * @return является ли число степенью двойки
-         */
-        static boolean isTwoGrade(int a) {
-            return (Math.abs(a) == Integer.highestOneBit(Math.abs(a))) ? true : false;
-        }
+    /**
+     * Метод реализующий решение задачи 5
+     * @return является ли число степенью двойки
+     */
+    static boolean isTwoGrade(int a) {
+        return (Math.abs(a) == Integer.highestOneBit(Math.abs(a))) ? true : false;
+    }
 
-        /**
-         * Метод реализующий решение задачи 6
-         * @return является ли строка палиндроомом
-         */
-        static boolean isPalindrome(String inputString) {
-            String filteredString = inputString.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
-            StringBuilder reverseFilteredString = new StringBuilder(filteredString).reverse();
-            return (filteredString.equals(reverseFilteredString.toString())) ? true : false;
-        }
+    /**
+     * Метод реализующий решение задачи 6
+     * @return является ли строка палиндроомом
+     */
+    static boolean isPalindrome(String inputString) {
+        String filteredString = inputString.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        StringBuilder reverseFilteredString = new StringBuilder(filteredString).reverse();
+        return (filteredString.equals(reverseFilteredString.toString())) ? true : false;
     }
 }
