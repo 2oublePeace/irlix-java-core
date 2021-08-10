fdcfa7dafe93187e507f19b523c8c3d6c3ff5052
2oublePeace <vova.emiryan@yandex.ru>
2021-08-10 21:01:01 +0400

    done 6 task

---
+++ src/com/stage3/MainApp.java
@@ -0,0 +1,14 @@
+package com.stage3;
+
+import java.util.*;
+
+public class MainApp {
+    public static void main(String[] args) {
+        //TaskSolving.task1(List.of(1,2,3), List.of(0,1,2));
+        //TaskSolving.task2();
+        //TaskSolving.task3("assafsffdsfd");
+        //TaskSolving.task4(new ArrayList(Arrays.asList(new Object[]{"string", "string", 1, '2', true, false, true})));
+        //TaskSolving.task5();
+        //TaskSolving.task6();
+    }
+}
